fcb98fc6f33a1af385a823c78d961ec551ea0eb7
Chrisanthi Tektonidou <chrissa199910@gail.com>
2019-05-30 10:31:29 +0300

    db1

---
+++ checkout.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 30, 2019 at 09:30 AM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.1.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `checkout`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `telos`
+--
+
+CREATE TABLE `telos` (
+  `fname` char(50) NOT NULL,
+  `lname` char(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `country` char(20) NOT NULL,
+  `city` char(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `tk` int(5) NOT NULL,
+  `phone` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `telos`
+--
+
+INSERT INTO `telos` (`fname`, `lname`, `email`, `country`, `city`, `address`, `tk`, `phone`) VALUES
+('gg', 'hh', 'se@gmail.com', 'dr', 'cf', 'xd 19', 65404, 5555);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
